7dc5d4a190cfa2dfdb980a46a36ed3ad7f712fc1
PatrickETaylor <patricketaylor1990@gmail.com>
2018-12-09 22:27:29 -0600

    Update index.js
    Better selector method

--- src/index.js
+++ src/index.js
@@ -10,4 +10,4 @@ const App = () => {
     return <div>Latitude: </div>
 }; 
 
-ReactDOM.render( <App />, document.querySelector('#root'));
+ReactDOM.render( <App />, document.getElementById('root'));
